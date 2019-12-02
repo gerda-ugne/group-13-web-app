cc1ac60288f616f2a0ce83748e5270ce81229ac8
AndrewStudders <43141205+AndrewStudders@users.noreply.github.com>
2019-12-02 05:15:28 +0000

    javascript & html page for calculator
    some bugs with making the html work

---
+++ calc.js
@@ -0,0 +1,65 @@
+
+Clear();
+var newtotal=0; x;score=0;
+
+
+function Glass()
+{
+x=newtotal;
+newtotal=x-2;
+document.getElementById('disp').innerHTML= newtotal;
+}
+function CardBoard()
+{
+x=newtotal;
+newtotal=x-3;
+document.getElementById('disp').innerHTML= newtotal;
+}
+function tins()
+{
+x=newtotal;
+newtotal=x-4;
+document.getElementById('disp').innerHTML= newtotal;
+}
+function plastic_bottles()
+{
+x=newtotal;
+newtotal=x-5;
+document.getElementById('disp').innerHTML= newtotal;
+}
+function drinks_cartons()
+{
+x=newtotal;
+newtotal=x+1;
+document.getElementById('disp').innerHTML= newtotal;
+}
+function plastic_bags()
+{
+x=newtotal;
+newtotal=x+2;
+document.getElementById('disp').innerHTML= newtotal;
+}
+function plastic_film()
+{
+x=newtotal;
+newtotal=x+3;
+document.getElementById('disp').innerHTML= newtotal;
+}
+function soiled_cardboard()
+{
+x=newtotal;
+newtotal=x+4;
+document.getElementById('disp').innerHTML= newtotal;
+}
+function crisp_packet()
+{
+x=newtotal;
+newtotal=x+5;
+document.getElementById('disp').innerHTML= newtotal;
+}
+function Clear()
+{
+x=newtotal;
+newtotal=0;
+document.getElementById('disp').innerHTML= newtotal;
+}
